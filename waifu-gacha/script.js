580cd7bae2e764dbd0fdcc9b97221e6fe756dd9a
firzaelbuho <firzaaditya1315@gmail.com>
2020-08-14 20:39:22 +0700

    v1 gacha

--- waifu-gacha/script.js
+++ waifu-gacha/script.js
@@ -1,20 +1,40 @@
 const img = document.getElementById('myImg')
 const btn = document.getElementById('btn')
 const detail = document.getElementById('detail')
-const tittlw = document.getElementById('tittlw')
+const tittle = document.getElementsByTagName('h2')[0]
+const loader = document.getElementById("load")
+let isClicked = false
 
-alert('hehe')
-img.style.visibility = 'hidden'
+
+
+
 img.style.height = "1px"
 
 
 btn.addEventListener('click', function () {
 
-    img.style.visibility = "visible"
-    img.style.height = "600px"
-    img.src = waifus[0][2]
-    detail.innerHTML = "<p>" + waifus[0][0] + "</p> <p>Rate : " + waifus[0][1] + "</p>"
-    tittlw.innerHTML = "Your Waifu is"
+    setTimeout(function () {
+
+        if (!isClicked) {
+            loader.style.visibility = "hidden"
+            img.style.visibility = "visible"
+            img.style.height = "600px"
+            img.src = waifus[x][2]
+            detail.innerHTML = "<p>" + waifus[x][0] + "</p> <p>Rate : " + waifus[x][1] + "</p>"
+            tittle.innerHTML = "Your Waifu is"
+            btn.innerHTML = "your waifu"
+            btn.classList.remove("btn-primary")
+            btn.classList.add("btn-secondary")
+            isClicked = true
+
+        }
+
+
+    }, 1000)
+
+    const x = getRandom() - 1
+    loader.style.visibility = "visible"
+
 
 
 })
@@ -22,4 +42,17 @@ btn.addEventListener('click', function () {
 
 const waifus = [
     ['Miyazono Kaori', 'SSR', 'https://intipanime.com/wp-content/uploads/2018/05/Before-1-1.png'],
+    ['Mikasa Ackerman', 'SSR', 'https://1.bp.blogspot.com/-r6OwY4VoZ8U/XWtFcDnjfsI/AAAAAAAACxI/uc7YAfVHJX0HTslbzuM_WxCcWPQ5KiA8gCLcBGAs/s1600/Attack%2Bon%2BTitan%2BS2%2B-%2B02.jpg'],
+    ['Meiko "Menma" Honma', 'SR', 'https://i.ytimg.com/vi/DiElnzXFqLc/maxresdefault.jpg'],
+    ['Zero Two', 'S', 'https://c4.wallpaperflare.com/wallpaper/566/52/952/anime-anime-girls-darling-in-the-franxx-zero-two-darling-in-the-franxx-wallpaper-preview.jpg'],
+    ['Hyuuga Hinata', 'SR', 'https://lh3.googleusercontent.com/proxy/nNUcoDAYxP_miOJi8dNwgk8MvkcQLZoa8fUVbgSlxKUv1iCJ313qRO-fDAB8rjpTYsVdg0p98zWaTZ0kpXAMtdiNIL6sZQAqZqYqucHiSUnhyxzp8FKOcelpX4B14CyKWaq3kM49uEMeswxzWCKVLGDbW30SLcR3yA'],
+    ['Haruno Sakura', 'B', 'https://cdn.idntimes.com/content-images/post/20200417/2-52f72ba097bed7a8356bb2aa02221d80_600x400.jpg'],
+    ['Aqua', 'SSR', 'https://i.ytimg.com/vi/ZZ060Fv74sY/maxresdefault.jpg']
 ]
+
+function getRandom() {
+
+    return Math.round(Math.random() * waifus.length)
+
+}
+
