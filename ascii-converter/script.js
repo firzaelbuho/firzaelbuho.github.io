64066705b07ab1662b78bc9a83ebaa27e534c12f
firzaelbuho <firzaaditya1315@gmail.com>
2020-09-19 09:47:42 +0700

    add ascii converter

---
+++ ascii-converter/script.js
@@ -0,0 +1,17 @@
+const input = document.getElementById("inputField")
+const output = document.getElementById("outputField")
+const convert = document.getElementById("convertBtn")
+
+convert.addEventListener('click',
+    function () {
+
+        const inputValue = input.value
+        let outputArray = []
+
+        //alert(outputValue)
+        for (var i = 0; i < inputValue.length; i++) {
+            outputArray.push(inputValue.charCodeAt(i))
+        }
+        const outputString = outputArray.join(' ')
+        output.value = outputString
+    })
