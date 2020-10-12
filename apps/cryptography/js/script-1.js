4e0338d8a65bc3e791ec0afe58bece1356754c32
firzaelbuho <firzaaditya1315@gmail.com>
2020-10-13 00:01:45 +0700

    finish crypto 1

--- apps/cryptography/js/script-1.js
+++ apps/cryptography/js/script-1.js
@@ -2,58 +2,166 @@ let keyNode = document.getElementById("key")
 let plainNode = document.getElementById("plain")
 let cipherNode = document.getElementById("cipher")
 let btnNode = document.getElementById("encryptBtn")
+let modeNode = document.getElementById("mode")
+let mode = modeNode.value
 
-let arrayOfAlphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+let arrayOfAlphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+    'u', 'v', 'w', 'x', 'y', 'z', ' ']
 
-plainNode.value = "masukkan plain text"
+
+
+// reset plain text
+
+
+plainNode.value = ""
+
+// on changing mode
+
+modeNode.addEventListener('change', function () {
+    mode = modeNode.value
+    if (mode === "enkripsi") {
+        plainNode.placeholder = "mauskan plaintext .."
+    } else {
+        plainNode.placeholder = "mauskan ciphertext .."
+    }
+    btnNode.innerHTML = "Mulai Dekripsi"
+})
+
+// on btn submit
 
 btnNode.addEventListener('click',
+
     function () {
-        let plainText = plainNode.value
-        let arrayOfCipher = []
+
+        let rawText = plainNode.value.toLowerCase()
+        plainNode.value = rawText
+
         let key = parseInt(keyNode.value) % 26
-        for (var i = 0; i < 26; i++) {
 
-            let index = key + i
 
-            if ((index) > 25) {
-                index = index % 26
-            }
-            arrayOfCipher.push(arrayOfAlphabets[index])
 
+        // check mode
+
+        let arrayOfResult = []
+
+
+        if (mode === "enkripsi") {
+            arrayOfResult = getEncrypt(key)
+        } else {
+            arrayOfResult = getDecrypt(key)
         }
-        encryptonProcces(plainText, arrayOfCipher)
+
+        // add space to arrayOfResult
+
+        arrayOfResult.push(' ')
+
+
+
+        // get result string
+
+        let result = getResult(rawText, arrayOfResult)
+
+
+
+        cipherNode.value = result
 
     }
 )
 
 
+// encryption proccess
 
-function encryptonProcces(plainText, arrayOfCipher) {
+function getEncrypt(key) {
+
+    // make new list Alphabet after encrypted
+
+    let arrayOfCipher = []
+    for (var i = 0; i < 26; i++) {
+
+        let index = i + key
+
+        if ((index) > 25) {
+            index = index % 26
+        }
+        arrayOfCipher.push(arrayOfAlphabets[index])
+
+
+
+
+
+    }
+    return arrayOfCipher
+
+}
+
+// decryption proccess
+
+function getDecrypt(key) {
+
+    // make new list Alphabet after decrypted
+
+    let arrayOfPlain = []
+    for (var i = 0; i < 26; i++) {
+
+        let index = i - key
+
+        if ((index) < 0) {
+            index = index + 26
+        }
+        arrayOfPlain.push(arrayOfAlphabets[index])
+
+
+
+    }
+    return arrayOfPlain
+}
+
+function getResult(rawText, arrayOfResult) {
+
+    // make array of result index each characters
 
     let arrayOfIndex = []
 
-    for (var i = 0; i < plainText.length; i++) {
-        let charValue = plainText.charAt(i)
-        let index = arrayOfAlphabets.indexOf(charValue)
+    for (var i = 0; i < rawText.length; i++) {
+
+
+
+        // get value each index of string
+        let charValue = rawText.charAt(i)
+
+        // get the index of value in arrayOfAlphabet
+        let index = -1
+
+        // check the char is space or not 
+        if (charValue === ' ') {
+            index = 26
+
+        } else {
+            index = arrayOfAlphabets.indexOf(charValue)
+        }
+
         arrayOfIndex.push(index)
+
 
 
     }
 
     let preCipherText = []
 
+    // convert the array of index of rawtext into result text
     for (var i = 0; i < arrayOfIndex.length; i++) {
         let index = arrayOfIndex[i]
-        let value = arrayOfCipher[index]
+        let value = arrayOfResult[index]
         preCipherText.push(value)
 
 
     }
 
-    let cipherText = preCipherText.toString("")
+    // join the array into string
 
-    cipherNode.value = cipherText
+    let resultText = preCipherText.join("")
+
+    return resultText
 
 
 
